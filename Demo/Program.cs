4a6e85b8e38e67d370266b0d2b491a777d7fcd5d
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-30 22:56:46 +0200

    Struct

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,6 +5,30 @@
         static void Main(string[] args)
         {
 
+            #region Struct
+            //Struct : Value Type : STACK
+            //Point P01;
+            ////Declear For Object From Typr Point
+            ////PO01 : Object
+            ////Allocate Uninitialized 8 Bytes At STACK
+
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);//unassigned
+
+            ////P01.X = 12;
+            ////P01.Y = 12;
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+
+            // P01=new Point(1,2 );
+
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+
+            ////Console.WriteLine(P01);
+            //P01.Print(); 
+            #endregion
+
         }
     }
 }
